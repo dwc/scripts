5002194aeae1e5fe3300015191a78d820f085f2c
Daniel Westermann-Clark <dwc@pobox.com>
2013-11-17 17:10:38 -0800

    Update

--- publish_tv_calendar.sh
+++ publish_tv_calendar.sh
@@ -9,12 +9,11 @@ HOST=files.danieltwc.com
 DIRECTORY=files.danieltwc.com/
 
 tv_grep \
-	--or --title "^Boardwalk\s+Empire$" \
+	--title "^Boardwalk\s+Empire$" \
 		--and --channel "I10240\.labs\.zap2it\.com" \
 	--or --title "^Bored\s+to\s+Death$" \
-	--or --title "^Breaking\s+Bad$" \
-	--or --title "^Dexter$" \
-		--and --channel "I11117\.labs\.zap2it\.com" \
+	--or --title "^Continuum$" \
+		--and --not --channel "I65626\.labs\.zap2it\.com" \
 	--or --title "^How\s+I\s+Met\s+Your\s+Mother$" \
 		--and --channel "I10520\.labs\.zap2it\.com" \
 	--or --title "^Lie\s+to\s+Me$" \
@@ -22,14 +21,16 @@ tv_grep \
 	--or --title "^Louie$" \
 		--and --not --channel "I14753\.labs\.zap2it\.com" \
 	--or --title "^Mad\s+Men$" \
+		--and --not --channel "I31556\.labs\.zap2it\.com" \
 	--or --title "^Parks\s+and\s+Recreation$" \
 		--and --channel "I10518\.labs\.zap2it\.com" \
+	--or --title "^The\s+Blacklist$" \
+		--and --not --channel "I19630\.labs\.zap2it\.com" \
+	--or --title "^The\s+Layover\s+With\s+Anthony\s+Bourdain$" \
 	--or --title "^The\s+Walking\s+Dead$" \
 		--and --not --channel "I59337\.labs\.zap2it\.com" \
 	--or --title "^Top\s+Chef" \
 		--and --not --channel "I31555\.labs\.zap2it\.com" \
-	--or --title "^Top\s+Gear" \
-		--and --not --channel "I18332\.labs\.zap2it\.com" \
 $INPUT_FILE | $HOME/bin/tv_to_ical --no-old > $OUTPUT_FILE
 
 scp $OUTPUT_FILE $USER@$HOST:$DIRECTORY
